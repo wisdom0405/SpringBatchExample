3605b7a6f60e391802a82b806ee51af4d8ae330e
wisdom0405 <wisdom0405@kakao.com>
2025-05-07 00:29:41 +0900

    feat : BeforeEntity, AfterEntity, Repository 생성

---
+++ src/main/java/com/example/SpringBatchExample/repository/BeforeRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBatchExample.repository;
+
+import com.example.SpringBatchExample.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+
+}
