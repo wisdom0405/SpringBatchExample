3ec05f08737eb26ec0aed5437a313e0b82f6a88a
wisdom0405 <wisdom0405@kakao.com>
2025-05-07 02:08:17 +0900

    fix : 스케쥴러비활성화

--- src/main/java/com/example/SpringBatchExample/SpringBatchExampleApplication.java
+++ src/main/java/com/example/SpringBatchExample/SpringBatchExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // 스케쥴링 어노테이션 활성화 시켜줌
+//@EnableScheduling // 스케쥴링 어노테이션 활성화 시켜줌
 public class SpringBatchExampleApplication {
 
 	public static void main(String[] args) {
